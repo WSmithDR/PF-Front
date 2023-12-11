cd88a96764671e6fa367559be059c0fd6585a469
XliazZz <silentcd1912@gmail.com>
2023-12-11 20:30:00 -0300

    cambios

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -7,6 +7,7 @@ import Welcome from './Welcome';
 const Home = () => {
   const info = useSelector((state) => state.products?.info);
   const products = useSelector((state) => state.products.results);
+  console.log(products);
   return (
     <div className="relative h-full min-h-[100vh] bg-blue-200 inset-0 text-center pt-5 pb-0 flex flex-col items-center">
       <Welcome/>
