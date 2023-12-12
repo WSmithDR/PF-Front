4e33ceb92bf08880346f07ab56778b058fb656c5
WagnerSmith <wagnersmith123@hotmail.com>
2023-12-12 17:16:38 -0500

    estilizando

--- src/views/Admin/CreateProduct.jsx
+++ src/views/Admin/CreateProduct.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import validation from '../../utils/Validation/validationCreateProducts';
 import axios from "axios";
+import { useState } from "react";
 import Swal from 'sweetalert2';
+import validation from '../../utils/Validation/validationCreateProducts';
 
 const CreateProduct = () => {
 
@@ -107,7 +107,8 @@ const CreateProduct = () => {
   }
 
   return (
-    <div className="pl-72 ml-10 pr-10 p-9 pb-10 pt-10 min-h-[80vh] bg-gray-900">        
+    <div className="p-5 pt-10 bg-gray-900 ml-72 h-full min-h-[100vh]">
+    <div className="p-5 rounded-xl bg-gray-950 shadow mt-9 h-full">        
       <form
         encType="multipart/form-data"
         onSubmit={handleSubmit}
@@ -306,6 +307,7 @@ const CreateProduct = () => {
           </div>
         </div>
       </form>
+    </div>
     </div>
   );
 };
